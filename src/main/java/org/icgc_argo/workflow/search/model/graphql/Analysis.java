4438e237b4237005be8783ce9f9fbbd6f267ea1d
Alex <lepsalex@gmail.com>
2020-06-15 18:16:54 -0400

    Analysis POJO

---
+++ src/main/java/org/icgc_argo/workflow/search/model/graphql/Analysis.java
@@ -0,0 +1,31 @@
+package org.icgc_argo.workflow.search.model.graphql;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class Analysis {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private String analysisId;
+
+    private List<Run> runs;
+
+    @SneakyThrows
+    public static Analysis parse(@NonNull Map<String, Object> sourceMap) {
+        return MAPPER.convertValue(sourceMap, Analysis.class);
+    }
+
+}
